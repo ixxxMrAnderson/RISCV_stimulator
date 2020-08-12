84ed9a29fc514a4858051ce8fdd5baf8bc427f85
CHTVII <chtvii@sjtu.edu.cn>
2020-08-12 09:33:51 +0800

    main

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include <fstream>
 long long total_loop = 0;
 int main() {
-    freopen("D:\\ClionProjects\\RISCV\\test.txt", "r", stdin);
+//    freopen("D:\\ClionProjects\\RISCV\\test.txt", "r", stdin);
     READIN();
-    freopen("CON", "r", stdin);
+//    freopen("CON", "r", stdin);
     MEM_.state = VACANT;
     while (1){
         WB();
@@ -18,9 +18,9 @@ int main() {
         total_loop ++;
         if (!FORWARD()) break;
     }
-    printf("return:\t%d\n", reg[10]);
-    printf("cal:\t%lld\n", cal_tot);
-    printf("jump:\t%lld\n", jump_tot);
-    printf("mem:\t%lld\n", mem_tot);
+//    printf("return:\t%d\n", reg[10]);
+//    printf("cal:\t%lld\n", cal_tot);
+//    printf("jump:\t%lld\n", jump_tot);
+//    printf("mem:\t%lld\n", mem_tot);
     //printf("correct_prediction: %d/%d (%.2f%)", correct, total, double(correct)/double(total)*100);
 }
